a8b38c0ea0a552e99741eea73e47348af8657e0e
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-16 03:41:51 -0700

    add randomizeTime function

--- src/js/timeService.js
+++ src/js/timeService.js
@@ -14,6 +14,29 @@ export default class TimeService {
     const triggerTimeMs = eventTime.getTime() - now.getTime();
     setTimeout(event, triggerTimeMs);
   }
+  
+  //scale is either 0 no random, 1 for minutes random, or 2 for completely random
+  randomizeTime(time, scale) {
+    let hour = Number(time.split(':')[0]);
+    let minute = Number(time.split(':')[1]);
+    const randomValue = Math.random();
+
+    if (scale == 0) {
+      return time;
+    } else if (scale == 1) { 
+      minute = parseInt(randomValue*30);
+    } else {
+      minute = parseInt(randomValue*60);
+      hour = parseInt(randomValue*23);
+    }
+
+    if (minute < 10) {
+    minute = ("0" + minute).slice(-2);
+    }
+    
+    const randTime = [hour,minute].join(":");
+      return randTime;
+    }
 
 
 }
