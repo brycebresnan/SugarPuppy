e3d804ee34c86d810b06ffa1ca0505b0533db366
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-17 16:53:37 -0700

    update test code

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import Score from "./js/score.js";
 import Day from "./js/day.js";
 
 // *****************For Testing Timer********************
-const array = [["0:24",alarm],["0:22",alarm],["0:23",alarm]];
-let day = new Day(0, array);
+const array = [["16:50",alarm],["16:51",alarm],["16:52",alarm]];
+let day = new Day(1, array);
 
 running();
 
