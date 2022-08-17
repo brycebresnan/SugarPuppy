5f2fad5e44eb0f920404282d6acb686e763b45f1
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-17 00:26:32 -0700

    removed currentTime property and refactor

--- src/js/day.js
+++ src/js/day.js
@@ -1,16 +1,17 @@
+import TimeService from "./timeService.js";
+
 export default class Day {
   constructor(dayNumber, eventsList) {
     this.dayNumber = dayNumber;
     this.eventsList = eventsList;
-    this.currentTime = new Date();
-    this.eventLog = []
+    this.eventLog = [];
   }
 
   startDay(){
     const eventArray = this.eventsList;
-    eventArray.forEach(item, function(){
+    eventArray.forEach(item => {
       let time = item[0];
-      const rTime = TimeService.randomizeTime(time, 1);
+      const rTime = TimeService.randomizeTime(time, 0);
       TimeService.timer(rTime, item[1]);
     });
   }
