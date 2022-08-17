ca4592c33b5f282f34f1f675b5d87c06d2300d44
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-16 21:22:24 -0700

    add import for TimeService and test for timer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import TimeService from "./js/timeService.js";
+
+//*****************For Testing Timer********************
+// running();
+
+// function running(){
+//   console.log("running!");
+//   TimeService.timer("20:55", alarm);
+// }
+
+// function alarm() {
+//   window.alert("It's Time!");
+// }
+
+//*****************For Testing Timer********************
