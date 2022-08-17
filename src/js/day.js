344734c9f2ef2b999beab294cb6f3a8202d70c7e
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-17 16:53:19 -0700

    FIX: startDay() now accounts for day number

--- src/js/day.js
+++ src/js/day.js
@@ -12,7 +12,7 @@ export default class Day {
     eventArray.forEach(item => {
       let time = item[0];
       const rTime = TimeService.randomizeTime(time, 0);
-      TimeService.timer(rTime, item[1]);
+      TimeService.timer(rTime, item[1], this.dayNumber);
     });
   }
 }
