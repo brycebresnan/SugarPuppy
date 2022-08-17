1fd9f34ec4e20f415e73f1584359480ee0733e9b
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-16 23:17:03 -0700

    add Score class and calculateScore function

---
+++ src/js/score.js
@@ -0,0 +1,17 @@
+export default class Score {
+  constructor() {
+    this.score = 100;
+  }
+
+  calculateScore(time){
+    if (time < 10) {
+      return;
+    } else if (time > 11 && time < 30) {
+      this.score - 5;
+    } else if (time > 31 && time < 60) {
+      this.score - 10;
+    } else {
+      this.score - 25; 
+    }
+  }
+}
