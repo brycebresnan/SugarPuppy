52e4ab60b980f03b5e355212c86e78fbad10ff4a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-18 01:38:50 -0700

    testing matterial for stopwatch

--- src/index.js
+++ src/index.js
@@ -6,19 +6,33 @@ import Score from "./js/score.js";
 import Day from "./js/day.js";
 
 // *****************For Testing Timer********************
-const array = [["16:50",alarm],["16:51",alarm],["16:52",alarm]];
-let day = new Day(1, array);
+// const array = [["16:50",alarm],["16:51",alarm],["16:52",alarm]];
+// let day = new Day(1, array);
 
-running();
+// running();
 
-function running(){
-  console.log("running!");
-  day.startDay();
-}
+// function running(){
+//   console.log("running!");
+//   day.startDay();
+// }
 
-function alarm() {
-  window.alert(`It's Time!`);
+// function alarm() {
+//   window.alert(`It's Time!`);
 
-}
+// }
 
 // *****************For Testing Timer********************
+
+
+// *****************For Testing StopWatch********************
+
+// const stopWatch = new TimeService();
+
+// run();
+
+// function run(){
+//   stopWatch.startWatch();
+//   setTimeout(function(){stopWatch.stopWatch()}, 2000);
+//   setTimeout(function(){stopWatch.resetWatch()}, 3000);
+// }
+// *****************For Testing StopWatch********************
