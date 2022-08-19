7cef540bcc0dac2b070b211944332a6c7eae0b16
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-18 19:48:01 -0700

    FIX: operators changed to modify score properly

--- src/js/score.js
+++ src/js/score.js
@@ -1,17 +1,18 @@
 export default class Score {
   constructor() {
     this.score = 100;
+    this.totalScore = 1000;
   }
 
   calculateScore(time){
     if (time < 10) {
       return;
     } else if (time > 11 && time < 30) {
-      this.score - 5;
+      this.score -= 5;
     } else if (time > 31 && time < 60) {
-      this.score - 10;
+      this.score -= 10;
     } else {
-      this.score - 25; 
+      this.score -= 25; 
     }
   }
 
