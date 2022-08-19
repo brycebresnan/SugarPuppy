d0c1dc94d6393540267f5ec3d0551c0db10c59d6
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-18 21:14:30 -0700

    create simulator class

---
+++ src/js/simulator.js
@@ -0,0 +1,14 @@
+import TimeService from "./timeService.js";
+import Score from "./score.js";
+import Day from "./day.js";
+import Event from './events';
+
+export default class Simulator {
+  constructor() {
+    this.score = new Score();
+    this.events = new Event();
+    this.day = new Day();
+    this.eventLog = [];
+  }
+
+}
