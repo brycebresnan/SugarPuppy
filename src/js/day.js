c70216296e4e2c8bc868b4d8a1584f34d943840d
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-19 16:26:19 -0700

    remove import statements, cleanup

--- src/js/day.js
+++ src/js/day.js
@@ -1,13 +1,7 @@
-import TimeService from "./timeService.js";
-import Event from "./event.js";
 
 export default class Day {
   constructor(dayNumber, eventsList) {
     this.dayNumber = dayNumber;
-    this.eventsList = [];
+    this.eventsList = eventsList;
   }
-
-  
-
-  
 }
