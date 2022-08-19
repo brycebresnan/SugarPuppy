07d0056460b6778564d285bd126f699f7d0ea4c6
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-19 16:26:05 -0700

    refactor to simulator function. Builds template

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,3 @@
-import TimeService from "./timeService";
-import Simulator from "./simulator";
-import Score from "./score";
 
 export default class Event {
 
@@ -15,26 +12,14 @@ export default class Event {
     });
   }
 
-  async walk() {
-    let stopWatch = new TimeService();
-    stopWatch.startWatch(); //Starts the timer for the event
-
+  static morningWalk() {
+    const time = "7:00";
     const eventTitle = "Walk the Dog";
     const eventText = "Description of the event";
     const items = ["Leash","Collar"]; //items required for the event
     // if (searchItems(items) === false){buyItems(items)}; //search for items, if not found, promt user to buy items
-    
-    let element = document.querySelector("button"); //change button name. Might have to pass into function later.
-    await Event.clickListener(element,"click", stopWatch);
 
-    Score.calculateScore(stopWatch.duration);
-    const timeStamp = new Date();
-    let logArray = [timeStamp, eventTitle]; //find a way to package score
-    this.eventLog.push(logArray); //find a way to push to eventLog in Simulator. Return?
-    console.log(this.eventLog);
-
-    returnObj = {"eventTitle":eventTitle, "eventText":eventText}
+    return {"time":time,"eventTitle":eventTitle, "eventText":eventText, "items":items};
   }
 
-  
 }
