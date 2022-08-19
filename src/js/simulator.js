850b34b41b72a8bd75041028cda6eaad6df5e94c
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-19 12:10:38 -0700

    refactor to move functions into simulator

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -1,14 +1,31 @@
 import TimeService from "./timeService.js";
 import Score from "./score.js";
 import Day from "./day.js";
-import Event from './event';
+import Event from "./event.js";
 
 export default class Simulator {
   constructor() {
     this.score = new Score();
-    this.events = new Event();
-    this.day = new Day();
     this.eventLog = [];
   }
+
+  // packager() {
+  //   const day = new Day();
+  //   const events = new Event();
+  //   const eventList = [["7:00",events.walk]];
+  //   day.eventsList = eventList;
+  //   day.startDay();
+  // }
+
+  start(){
+    const events = new Event();
+    const eventList = [["12:01",events.walk]];
+    const day = new Day(1,eventList);
+    const score = new Score();
+    day.startDay();
+    console.log(this.eventLog())
+    
+  }
+
 
 }
