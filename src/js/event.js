7a317e6d08bca6b61df4ea2282eb0714ee5c99e9
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-20 20:56:05 -0700

    refactor events to be params, add package function

--- src/js/event.js
+++ src/js/event.js
@@ -1,40 +1,53 @@
 
+
 export default class Event {
   constructor() {
-    this.mornWalk = this.morningWalk.bind(this);
-    this.evenWalk = this.eveningWalk.bind(this);
+    this.morningWalk = {
+      time:"7:00",
+      eventTitle:"Walk the Dog",
+      eventText:"Description of the event",
+      items:["Leash","Collar"]
+    };
+    this.eveningWalk = {
+      time:"10:00",
+      eventTitle:"Walk the Dog",
+      eventText:"Description of the event",
+      items:["Leash","Collar"]
+    };
   }
 
-  static clickListener(element, listenerName, stopWatch) {
-    return new Promise(function (resolve, reject) {
-      let listener = event => {
-        stopWatch.stopWatch();
-        element.removeEventListener(listenerName, listener); 
-        resolve(event);
-      };
-      element.addEventListener(listenerName, listener);
-    });
+  static eventPackager(eventObj) {
+    let keysArray = Object.keys(eventObj);
+    let eventArray = []
+    keysArray.forEach((item, index) => {
+      eventArray.push([eventObj[item].time, eventObj[item]]);
+  });
+
+    console.log(eventArray);
   }
 
-  static morningWalk() {
-    const time = "7:00";
-    const eventTitle = "Walk the Dog";
-    const eventText = "Description of the event";
-    const items = ["Leash","Collar"]; //items required for the event
-    // if (searchItems(items) === false){buyItems(items)}; //search for items, if not found, promt user to buy items
 
-    return {"time":time,"eventTitle":eventTitle, "eventText":eventText, "items":items};
-  }
 
-  static eveningWalk() {
-    const time = "19:00";
-    const eventTitle = "Walk the Dog";
-    const eventText = "Description of the event";
-    const items = ["Leash","Collar"]; //items required for the event
-    // if (searchItems(items) === false){buyItems(items)}; //search for items, if not found, promt user to buy items
 
-    return {"time":time,"eventTitle":eventTitle, "eventText":eventText, "items":items};
-  }
+  // static morningWalk() {
+  //   const time = "7:00";
+  //   const eventTitle = "Walk the Dog";
+  //   const eventText = "Description of the event";
+  //   const items = ["Leash","Collar"]; //items required for the event
+  //   // if (searchItems(items) === false){buyItems(items)}; //search for items, if not found, promt user to buy items
+
+  //   return {"time":time,"eventTitle":eventTitle, "eventText":eventText, "items":items};
+  // }
+
+  // static eveningWalk() {
+  //   const time = "19:00";
+  //   const eventTitle = "Walk the Dog";
+  //   const eventText = "Description of the event";
+  //   const items = ["Leash","Collar"]; //items required for the event
+  //   // if (searchItems(items) === false){buyItems(items)}; //search for items, if not found, promt user to buy items
+
+  //   return {"time":time,"eventTitle":eventTitle, "eventText":eventText, "items":items};
+  // }
 
   static morningFood() {
     const time = "8:00";
