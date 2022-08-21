114ec0cb7b53874115f0b112580c4f60f2e8e931
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-21 02:54:30 -0700

    add eventBuy frame. remove redudant variable

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -64,7 +64,6 @@ export default class Simulator {
       const modal = document.getElementById("myModal");
 
       const infoObject = this.eventHold;
-      const score = this.score;
 
       modal.style.display = "none"; //close modal
 
@@ -73,12 +72,18 @@ export default class Simulator {
       document.getElementById("eventText").innerText = null;//display text
 
       this.score.calculateScore(this.stopWatch.duration);
-      this.stopWatch.resetWatch()
+      this.stopWatch.resetWatch();
 
       const timeStamp = new Date(); //could package date better. Little long right now
       let logArray = [`${timeStamp.toString()}, ${infoObject["eventTitle"]}, Score = ${this.score.score}`]; //find a way to package score
       this.eventLog.push(logArray);
     }
+  }
+
+  eventBuy() {
+    //gets items from infoObject
+    //increments cost by item price
+    //pushes items to inventory
   }
 
 }
