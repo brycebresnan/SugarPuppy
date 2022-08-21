a4c4c9fdc3f03402da8562233fecdf942c57ce0e
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-20 23:20:14 -0700

    add Modal Popup template

--- src/index.js
+++ src/index.js
@@ -13,7 +13,33 @@ let element = document.getElementById("dispLog");
 let x = 0;
 element.addEventListener("click", () => document.getElementById("eventLog").innerText = x++ + sim.eventLog);
 
+// *****************For Modal Alert Testing********************
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// *****************For Modal Alert Testing********************
 
 // *****************For Testing Timer********************
 // const array = [["16:50",alarm],["16:51",alarm],["16:52",alarm]];
