f99c3ff203b76081b09b191adfba463b20e700f0
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-22 19:01:59 -0700

    FIX: replace span

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ scoreBtn.onclick = function () {
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+alertCloseBtn.onclick = function() {
   alertModal.style.display = "none";
 }
 introCloseBtn.onclick = function () {
